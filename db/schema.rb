2e230ab55d54169ee2dae42d58a8699bf7ac6a1d
松村早紀 <matsumurasaki@matsumurasakinoMacBook-Pro.local>
2024-10-07 17:16:19 +0900

    ユーザー登録機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_03_102408) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_06_140338) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,5 +22,15 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_03_102408) do
     t.boolean "show_remaining_time", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "name", null: false
+    t.string "crypted_password"
+    t.string "salt"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
