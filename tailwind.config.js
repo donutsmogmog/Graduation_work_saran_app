12cb84b11a38285ca376b3f536382c0c2db61f39
松村早紀 <matsumurasaki@matsumurasakinoMacBook-Pro.local>
2024-10-30 12:45:57 +0900

    ヘッダー作成

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,24 @@ module.exports = {
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
   ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF66A1', // テーマカラーに基づいた設定
+        secondary: '#FFC2D1',
+        accent: "#D946EF",     // アクセントカラー
+        "base-100": "#FFFFFF", // 基本の背景色
+        info: "#3ABFF8",
+        success: "#36D399",
+        warning: "#FBBD23",
+        error: "#F87272",
+      },
+      fontSize: {
+        base: '16px', // 基本のフォントサイズ
+        lg: '18px',
+      },
+    },
+  },
   plugins: [require('daisyui')],
   daisyui: {
     themes: ['valentine'],
